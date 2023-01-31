cd512f942c0771d47731427f13bc804764e56882
BradleyParkerDev <bradleyscottparker@outlook.com>
2023-01-31 13:38:12 -0600

    added blogs route, updated users route to return json

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const logger = require('morgan');
 //setup router for each set of routes
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const blogsRouter = require('./routes/blogs');
 
 // const port = 3001;
 //instatiate the actual express app
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //we bind (associate) the routers to routes in our application
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/blogs', blogsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/blogs.js
+++ routes/blogs.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('users page');
+  //res.send('blogs page');
+  res.send(res.json({success: true, route: "blogs", message:"Welcome to the blogs page."}));
 });
 
 module.exports = router;
