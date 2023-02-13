849b977993b3526efe4fb74eee3725d68ffbe3fb
BradleyParkerDev <bradleyscottparker@outlook.com>
2023-02-13 12:03:01 -0600

    done

--- routes/blogs.js
+++ routes/blogs.js
@@ -173,3 +173,150 @@ router.put("/update-one/:title", async (req,res)=>{
 
 
 module.exports = router;
+
+
+/*
+
+/////////////////////////////////////////////////////////////////////////////
+//Below are my non-persistent requests
+/////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////
+//A get request for all titles
+router.get('/all',function(req,res,next){
+    res.json({ success: true, blogs: sampleBlogs });
+});
+/////////////////////////////////////////////////////////////////////////////
+// GET request for a single title
+router.get('/single/:title',function(req,res,next){
+    //res.send(res.json(sampleBlogs))
+    const singleBlog = sampleBlogs.find((blog)=>{
+        return blog.title === req.params.title
+    })
+    res.json({ 
+        success: true, 
+        blog: singleBlog 
+    });
+});
+/////////////////////////////////////////////////////////////////////////////
+// DELETE request for a single title
+router.delete('/delete/:title',function(req,res,next){
+    const blogTitleToDelete = req.params.title
+	
+    const blogIndex = sampleBlogs.findIndex((blog)=>{
+		return blog.title === blogTitleToDelete
+	})
+	
+    sampleBlogs.splice(blogIndex, 1)
+	
+    res.json({
+		success: true
+	})
+});
+/////////////////////////////////////////////////////////////////////////////
+//POST request for a new blog
+router.post("/create-one", (req, res) => {
+  //try block, for validation code
+  try {
+    // anticipate fields of our post request /create-one
+    // parse out request data to local variables
+    const title = req.body.title;
+    const text = req.body.text;
+    const author = req.body.author;
+
+    //create userData object fields
+    //create blogData object fields
+    const blogData = {
+      title,
+      text,
+@@ -118,8 +118,7 @@ router.post("/create-one", (req, res) => {
+      success: true,
+    });
+  } catch (e) {
+
+// In the catch block, we always want to do 2 things: console.log the error and respond with an error object
+  // In the catch block, we always want to do 2 things: console.log the error and respond with an error object
+    console.log(e);
+    res.json({
+			success: false,
+@@ -128,8 +127,81 @@ router.post("/create-one", (req, res) => {
+  }
+});
+/////////////////////////////////////////////////////////////////////////////
+//PUT request to update sampleBlogs array
+router.put("/update-one/:title", (req,res)=>{
+
+  try{
+    //creating a variable for blog title to update
+    const titleToUpdate = req.params.title
+
+    //iterating through the sampleBlogs array,and locating the blog with the blog title
+    const originalBlog = sampleBlogs.find((blog)=>{
+      return blog.title === titleToUpdate
+    })
+
+    //iterating through the sampleBlogs array,and locating the blog's index with the blog title
+    const originalBlogIndex = sampleBlogs.findIndex((Blog)=>{
+      return Blog.title === titleToUpdate
+    })
+
+    // JSON message for a blog that couldn't be located
+    if (!originalBlog) {
+      res.json({
+        success: false,
+        message: "Could not find Blog in sampleBlogs."
+      })
+      return
+    }
+
+    //Creating an updated blog object
+	  const updatedBlog = {}
+
+    if (req.body.title !== undefined){
+      updatedBlog.title = req.body.title
+    } else {
+      updatedBlog.title = originalBlog.title
+    }
+
+    if (req.body.text !== undefined){
+      updatedBlog.text = req.body.text
+    } else {
+      updatedBlog.text = originalBlog.text
+    }
+
+    if (req.body.author !== undefined){
+      updatedBlog.author = req.body.author
+    } else {
+      updatedBlog.author = originalBlog.author
+    }
+
+    //Using the blogData validator
+    const blogDataCheck = validateBlogData(updatedBlog);
+
+    //Transfering the original createdAt and pdating the lastModified
+    updatedBlog.createdAt = originalBlog.createdAt;
+    updatedBlog.lastModified = new Date();
+
+    sampleBlogs[originalBlogIndex] = updatedBlog
+    if (blogDataCheck.isValid === false) {
+			throw Error(blogDataCheck.message)
+    }
+
+
+  } catch (e) {
+    // In the catch block, we always want to do 2 things: console.log the error and respond with an error object
+      console.log(e);
+      res.json({
+        success: false,
+        error: String(e)
+      });
+    }
+
+    res.json({
+        success: true
+    })
+})
+
+
+*/
